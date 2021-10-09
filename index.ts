d888a61266989f73933ee944bd0be794a393de49
Mikkel Laursen <mlaursen03@gmail.com>
2021-10-08 18:57:15 -0600

    chore(dev-deps): bump everything to latest

--- index.ts
+++ index.ts
@@ -28,7 +28,8 @@ const parserOpts: ParserOptions = {
   headerPattern: /^(\w*)(?:\((.*)\))?: (.*)$/,
   headerCorrespondence: ["type", "scope", "subject"],
   noteKeywords: ["BREAKING CHANGE"],
-  revertPattern: /^(?:Revert|revert:)\s"?([\s\S]+?)"?\s*This reverts commit (\w*)\./i,
+  revertPattern:
+    /^(?:Revert|revert:)\s"?([\s\S]+?)"?\s*This reverts commit (\w*)\./i,
   revertCorrespondence: ["header", "hash"],
 };
 
